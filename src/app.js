b8674e14aef836ad4b1daa18497824621bb08529
Nasir <ibnabdiwahid9@gmail.com>
2023-03-13 21:13:20 -0700

    created a api using express.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,42 @@
-// import {v4 as uuidv4  } from 'uuid'; 
-const {v4: uuidv4} = require('uuid')
+const express = require('express');
+const { json } = require('express/lib/response');
+const app = express();
 
-console.log(uuidv4())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+const PORT = 3000;
+
+const  customers =  [
+    {
+        "name": "Nasir",
+        "industry" : "lemon peeler"
+
+    },
+    {
+        "name": "Faiza",
+        "industry" : "tictoc watcher"
+    },
+    {
+        "name": "joe",
+        "industry" : "gum chewer"
+
+    }
+]
+
+
+app.get('/', (req,res ) => {
+    res.send({"customers":customers[0].name})
+})  
+
+app.post('/' ,(req,res) => {
+    res.send()
+})
+
+app.post('/api/customers',(req,res) => {
+    console.log(req.body);
+})
+
+app.listen(PORT, () => {
+    console.log('App listening on port' + PORT);
+}) 
+
